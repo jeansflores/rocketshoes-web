ef2800661fd26cf7a8ce0b789a7b792c137fc179
Jean <jeansfloresof@gmail.com>
2019-09-25 14:47:36 -0300

    Reactotron + Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import './config/ReactotronConfig';
 
-import store from './store';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import Header from './components/Header';
 import Routes from './routes';
+import store from './store';
 import GlobalStyle from './styles/global';
 
 function App() {
